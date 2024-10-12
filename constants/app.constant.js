851eb7ad0df91cc56b7d33ed41639f47fd54c1ee
NishadAvnish <avnishkumar1st@gmail.com>
2024-10-12 23:20:25 +0530

    constants file

---
+++ constants/app.constant.js
@@ -0,0 +1,5 @@
+const DB_NAME = "FressKart";
+const passwordSaltCount = 10;
+
+
+export { DB_NAME, passwordSaltCount }
---
+++ constants/app.strings.js
@@ -0,0 +1,10 @@
+
+export class AppStrings {
+    static allParamsRequired = "All fields are required!";
+    static wrongCredential = "Email Or password is not correct.";
+    static serverError = "Server Error!";
+    static notAbleToCreateEntry = "Not able to create entry";
+    static noItemFound = "No any item found!";
+    static successful = "Successful!";
+
+}
